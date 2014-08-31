a073a2469a4cfb8f3d44ecd886d1d2301a1b1fbe
dmirosh <dm.miroshnychenko@gmail.com>
2014-08-31 18:13:20 +0400

    added pagination

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,30 @@
+"use strict"
 angular.module('SpringLogs', ['ui.bootstrap'])
-    .controller('MainCtrl', ["$scope", "$http", function ($scope, $http) {
+    .controller('MainCtrl', function ($scope, $http, $filter) {
         var allMappings = null;
+        $scope.currentPage = 1;
+        $scope.pageSize = 7;
+        $scope.totalMappings = 0;
+        $scope.sortOrder = "url";
+        $scope.reverse = false;
+
+
+//        watch for url search input
+        $scope.$watch("urlQuery", function (newQuery) {
+            $scope.mappings = $filter("urlFilter")(allMappings, newQuery);
+            $scope.currentPage = 1;
+            $scope.totalMappings = $scope.mappings.length;
+        });
+
         $http.get("api/mappings")
             .success(function (mappingsList) {
                 allMappings = $scope.mappings = mappingsList;
+                $scope.totalMappings = $scope.mappings.length;
             })
             .error(function () {
                 console.log("can't get data from server");
             });
-    }])
+    })
     .filter("urlFilter", function () {
         return function (items, query) {
             var pattern = new RegExp(query, "i");
@@ -16,4 +32,10 @@ angular.module('SpringLogs', ['ui.bootstrap'])
                 return pattern.test(item.url);
             });
         }
+    })
+    .filter('startFrom', function () {
+        return function (input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        };
     });
