a473a9c49a5fc92968f76f05b82bc021065b8b7a
dmirosh <dm.miroshnychenko@gmail.com>
2014-08-31 12:08:08 +0400

    added showing mapping info table

--- controllers/mappingInfoController.js
+++ controllers/mappingInfoController.js
@@ -3,10 +3,18 @@
         extractor = require("../utils/mapInfoExtractor"),
         _ = require("../public/vendor/underscore"),
         mongoose = require("../utils/mongoose"),
-        config = require("../config");
+        config = require("../config"),
+        MappingInfo = require("../models/mappingInfo");
 
 
     exports.getAll = function (req, res) {
+        return MappingInfo.find( function( err, mappingInfos ) {
+            if( !err ) {
+                return res.json( mappingInfos );
+            } else {
+                return log.error( err );
+            }
+        });
     };
 
 
--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,11 @@
 angular.module('SpringLogs', [])
-    .controller('MainCtrl', function ($scope) {
-        $scope.name = "Test";
-    });
+    .controller('MainCtrl', ["$scope", "$http", function ($scope, $http) {
+        var allMappings = null;
+        $http.get("api/mappings")
+            .success(function (mappingsList) {
+                allMappings = $scope.mappings = mappingsList;
+            })
+            .error(function () {
+                console.log("can't get data from server");
+            });
+    }]);
