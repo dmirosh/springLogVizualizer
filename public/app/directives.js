3e51ce885d6f62ce7453b574288c8bb82cd9c25c
dmirosh <dm.miroshnychenko@gmail.com>
2014-09-11 20:24:10 +0400

    improved bar chart. Added using d3-tooltip library

--- public/app/directives.js
+++ public/app/directives.js
@@ -1,28 +1,79 @@
 "use strict";
 angular.module('SpringLogs')
     .directive('barChart', function () {
-        function render(chartSelect, chartData) {
-            console.log("render total: " + chartData.length);
-            var chart = chartSelect.selectAll('div');
-            chart = chart.data(chartData);
-            chart.enter().append("div").attr("class", "chart");
-            chart.exit().remove();
-            chart.style("width", function (d) { return d.val + "%"; })
-                .text(function (d) {return d.name + " - " + d.val + "%"; });
-        }
         return {
-            replace: true,
-            template: '<div></div>',
+            replace: false,
+            template: '<svg></svg>',
             restrict: "E",
             scope: {
                 chartData: "=chartData"
             },
-            link: function ($scope, element) {
-                $scope.$watch("chartData", function (newData) {
-                    render(chart, newData);
+            link: function ($scope, element, attrs) {
+                var chart = d3.select(element[0]).select("svg");
+                chart.attr({
+                    width: attrs.width,
+                    height: attrs.height
                 });
-                var chart = d3.select(element[0]);
-                render(chart, $scope.chartData);
+                var margin = {top: 10, right: 20, bottom: 20, left: +attrs.leftMargin},
+                    width = attrs.width - margin.left - margin.right,
+                    height = attrs.height - margin.top - margin.bottom;
+                //creating painting rectangle
+                chart = chart.append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                //creating and adding tooltip
+                var tip = d3.tip()
+                    .attr('class', 'd3-tip')
+                    .direction('e')
+                    .offset([0, 10])
+                    .html(function(d) {
+                        return "<strong>Total:</strong> <span>" + d.val + "</span>";
+                    });
+                chart.call(tip);
+                // watching chart data changes
+                $scope.$watch("chartData", render);
+                render($scope.chartData);
+
+                function render(chartData) {
+                    chart.select("g").selectAll("*").remove(); //clear previous chart
+                    if(!chartData || !chartData.length) {
+                        return;
+                    }
+                    //creating x scale anx axis
+                    var xScale = d3.scale.linear()
+                        .domain([0, d3.max(chartData, function (d) { return d.val; })])
+                        .range([0, width]);
+                    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+                    //creating y scale and axis
+                    var yPadding = .3;
+                    var yScale = d3.scale.ordinal()
+                        .domain(_.map(chartData, function (d) {return d.name}))
+                        .rangeRoundBands([0, height], yPadding);
+                    var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+                    //adding x axis
+                    chart.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
+                    //adding y axis
+                    chart.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis);
+                    //draw bar
+                    var bars = chart.selectAll(".bar").data(chartData);
+                    bars.enter().append("rect")
+                        .attr({
+                            class: "bar",
+                            x: function (d) {return 10},
+                            y: function (d) {return yScale(d.name)},
+                            height: yScale.rangeBand(),
+                            width: function (d) {return xScale(d.val)}
+                        })
+                        .on('mouseover', tip.show)
+                        .on('mouseout', tip.hide);
+                    bars.exit().remove();
+                }
             }
         };
     });
