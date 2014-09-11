5f8e69eeaa989f93181bb2eb11f8a2bfdac7e277
dmirosh <dm.miroshnychenko@gmail.com>
2014-09-11 20:45:43 +0400

    moved preparing data for charts to service

--- public/app/services.js
+++ public/app/services.js
@@ -1,6 +1,10 @@
 "use strict";
 angular.module('SpringLogs')
     .service("mappingInfoService", function ($http, $q) {
+        var CHART_DATA_MAKERS = {
+            httpMethods: _createChartDataForHttpMethods,
+            controllers: _createChartDataForControllers
+        };
         var mappingsInfo = null; //cached list of all mappings
 
         this.getAllMappings = function () {
@@ -20,14 +24,34 @@ angular.module('SpringLogs')
         this.getFilteredMappings = function (filters) {
             var res = $q.defer();
             if (!mappingsInfo) {
-                this.getAllMappings().then(function () {
+                this.getAllMappings().then(function (mappingsInfo) {
                     res.resolve(_filter(mappingsInfo, filters));
+                    return mappingsInfo;
                 });
             } else {
                 res.resolve(_filter(mappingsInfo, filters));
             }
             return res.promise;
         };
+        this.getChartData = function(type) {
+            var res = $q.defer();
+            var chartDataMaker = CHART_DATA_MAKERS[type];
+            if(!chartDataMaker) {
+                res.reject();
+                return res.promise;
+            }
+
+            if (!mappingsInfo) {
+                this.getAllMappings().then(function (mappingsInfo) {
+                    res.resolve(chartDataMaker(mappingsInfo));
+                    return mappingsInfo;
+                });
+            } else {
+                res.resolve(chartDataMaker(mappingsInfo));
+            }
+            return res.promise;
+        };
+        /** private functions **/
         function _filter(mappingsList, filters) {
             if (!mappingsList) {
                 return mappingsList;
@@ -50,6 +74,25 @@ angular.module('SpringLogs')
                     controllersMatches = _.contains(filters.controllers, item.handlerClassShort);
                 }
                 return queryMatches && controllersMatches;
+            });
+        }
+
+        function _createChartDataForHttpMethods(mappingsList) {
+            //TODO make httpMethods normal array
+            var GETMethodLength = _.filter(mappingsList, function (mapping) {return mapping.httpMethods.join("").indexOf("GET") != -1}).length,
+                POSTMethodLength = _.filter(mappingsList, function (mapping) {return mapping.httpMethods.join("").indexOf("POST") != -1}).length;
+            GETMethodLength = isNaN(GETMethodLength) ? 0 : GETMethodLength;
+            POSTMethodLength = isNaN(POSTMethodLength) ? 0 : POSTMethodLength;
+            return [
+                {name: "GET", val: GETMethodLength},
+                {name: "POST", val: POSTMethodLength}
+            ];
+        }
+
+        function _createChartDataForControllers(mappingsList) {
+            var groupedByController = _.groupBy(mappingsList, "handlerClassShort");
+            return _.map(groupedByController, function (mappings, controller) {
+                return {name: controller, val: mappings.length};
             });
         }
     });
